efebd27e9b63b19a95c8d882eb31205af9f01b28
Tabatha Slatton <valbatha@gmail.com>
2020-10-07 11:27:42 -0600

    added edit form and edit and delete buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,33 @@
 class PostsController < ApplicationController 
 
-    # get '/posts' do 
-    #     @posts = Post.all
-    #     erb :index
-    #  end
+    get '/posts' do 
+        @user = current_user
+        @posts = Post.all
+        erb :index
+     end
 
-    # get '/posts/new' do
-    #     erb :"posts/new"
-    # end
+    
+    post '/posts' do
+        post = Post.new(user_id: current_user.id, theme: params[:theme], content: params[:content])
+        if post.save
+            redirect "/posts"
+        else
+            # flash error (we learned these 10/06)
+            redirect "/posts"
+        end
+    end
 
-    # get '/posts/:id' do
-    #     id = params[:id]
-    #     @post = Post.find_by_id(id)
-    #     erb :"posts/show"
-    # end
+    patch 'posts/:id' do
+        @post = Post.find_by_id(params[:id])
+        @post.theme = params[:theme]
+        @post.content = params[:content]
+        @post.save
+        redirect "/posts"
+    end
 
-    # post '/posts' do
-    #     post = Post.new(params)
-    #     if post.save
-    #         redirect "posts/#{post.id}"
-    #     else
-    #         redirect "posts/new"
-    #     end
-    # end
+    delete 'posts/:id' do
+        @post = Post.find_by_id(params[:id])
+        @post.destroy
+        redirect "/posts"
+    end
 end

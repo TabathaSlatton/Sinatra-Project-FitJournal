c61dd1a8b72b20ebefee17c00a941cf35ba19b40
Tabatha Slatton <valbatha@gmail.com>
2020-10-07 14:29:50 -0600

    added editing for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,27 @@ class UsersController < ApplicationController
     get '/users/:id' do
         @current_user = current_user
         @user = User.find_by_id(params[:id]) || @current_user
-        @posts = Post.all
+        @posts = @user.posts
         erb :"users/show"
+    end
+
+    get '/users/:id/edit' do
+        @user = User.find_by_id(params[:id])
+        if @user.id == current_user.id
+        erb :"users/edit"
+        else
+        redirect "/"
+        end
+    end
+
+    patch '/users/:id' do
+        @user = User.find_by_id(params[:id])
+        @user.first_name = params[:first_name]
+        @user.last_name = params[:last_name]
+        @user.username = params[:username]
+        @user.long_term_goal = params[:long_term_goal]
+        @user.save
+        redirect "/users/#{@user.id}"
     end
 
 end
